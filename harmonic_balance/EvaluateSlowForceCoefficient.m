be1b996198bca9eea9f7127f43dc5a7744872c63
tobiaswdm <weidemann.tobias@t-online.de>
2024-04-02 17:26:17 +0200

    Switch to complex Slowflow

--- harmonic_balance/EvaluateSlowForceCoefficient.m
+++ harmonic_balance/EvaluateSlowForceCoefficient.m
@@ -1,7 +1,6 @@
-function [F_hat_real,F_hat_imag]=...
-    EvaluateSlowForceCoefficient(Phi_hat,N,H,sys)
+function [F_hat]=EvaluateSlowForceCoefficient(Phi_hat,N,H,sys)
 
-% Evaluate the real an imaginary part of the slow nonlinear force
+% Evaluate the slow nonlinear force
 % in the time domain without scaling of 8*epsilon_a*r^2 / pi^2
 %
 % Phi_hat - Fourier coefficients of amplitude envelope
@@ -16,24 +15,24 @@ Phi = FrequencyTime(Phi_hat,N,'Freq_to_Time');
 rho = (2/pi) * (1-sys.eN) / (1+sys.eN);
 
 % Check if slow flow falls of SIM
-i_not_on_SIM = Phi<(sys.Gamma(1)* rho/sqrt(1+rho^2));
+i_not_on_SIM = abs(Phi)<(sys.Gamma(1)* rho/sqrt(1+rho^2));
 
 % If not: Correct to avoid error but give warning
 if any(i_not_on_SIM)
     warning('Solution not on SIM.')
-    Phi(i_not_on_SIM) = 1.0001*sys.Gamma(1)* rho/sqrt(1+rho^2);
+    Phi(i_not_on_SIM) = 1.0001*sys.Gamma(1)* rho/sqrt(1+rho^2)*...
+                        exp(1i*angle(Phi(i_not_on_SIM)));
 end
 
 % Triangle Wave Amplitude of absorber
-qahat = (sys.Gamma(1)+sqrt((1+rho^2)*Phi.^2 ...
-    - (sys.Gamma(1)*rho)^2))/(1+rho^2);
+qahat = (sys.Gamma(1)+sqrt((1+rho^2)* abs(Phi).^2 ...
+        - (sys.Gamma(1)*rho)^2))/(1+rho^2);
 
-% Real and imaginary part of slow force coefficient
-force_real = qahat.*(qahat-sys.Gamma(1))./Phi;
-force_imag = rho*(qahat.^2)./Phi;
+% Slow Force Coefficient
+force = qahat.*(qahat*(1-1i*rho)-sys.Gamma(1))./abs(Phi) .* ...
+        exp(1i*angle(Phi));
 
 % Fourier coefficients
-F_hat_real = FrequencyTime(force_real,H,'Time_to_Freq');
-F_hat_imag = FrequencyTime(force_imag,H,'Time_to_Freq');
+F_hat = FrequencyTime(force,H,'Time_to_Freq');
 
 end
--- harmonic_balance/SlowFlowResidual.m
+++ harmonic_balance/SlowFlowResidual.m
@@ -2,17 +2,17 @@ function [residual] = SlowFlowResidual(coeff_ReIm,H,k_mod,sys,exc)
 %SLOWFLOWRESIDUAL Get the Harmonic Balance residual for the 
 % almost quasi periodic response (AQPR)
 %
-% H - Hamronic order
+% H - Harmonic order
 % coeff_ReIm - [2*(2*H+1),1] Real and Imaginary parts of complex Fourier
-%                coefficients of nonlinear force
-%               [Phi_0; Phi_1_Re; Phi_2_Re; ...; Phi_H_Re;
-%                Phi_1_Im; Phi_2_Im; ...; Phi_H_Im;
-%                Gamma_0; Gamma_1_Re; Gamma_2_Re; ...; Gamma_H_Re;
-%                Gamma_1_Im; Gamma_2_Im; ...; Gamma_H_Im]
+%                coefficients of slow flow
+%               [Phi_-H_Re; ...; Phi_0_Re; ...; Phi_H_Re;
+%                Phi_-H_Im; ...; Phi_0_Im; ...; Phi_H_Im]
 %
-% H - Hamronic order
+% H - Harmonic order
 % k_mod - Wavenumber of slow modulation
 
+% Fast excitation frequency
+r = exc.harmonic.r;
 
 % Modulation wavenumber phase angle
 theta_k_mod = 2*pi*k_mod/sys.N_s;
@@ -27,63 +27,38 @@ Phi_hat = coeff_ReIm(1:(2*H+1))+1i*coeff_ReIm((2*H+1)+(1:(2*H+1)));
 % Transform to time domain
 % chose at least 1000 sampling points per slow period
 N = max(1000,4*(H+1));
-% Slow flow in time domain
-Phi = FrequencyTime(Phi_hat,N,'Freq_to_Time');
 
 % Evaluate nonlinear dispersion relation and group velocity to get
 % slow modulation frequency Omega
-[~,Omega] = NonlinearDispersionRelation(coeff_ReIm(1),theta_k_0,sys);
+[~,Omega] = NonlinearDispersionRelation(abs(Phi_hat(H+1)),theta_k_0,sys);
 
 % Evaluate the real an imaginary part of the slow nonlinear force
 % in the time domain and get Fourier coefficients of dynamic components 
 % force coefficients without scaling of 8*epsilon_a*r^2 / pi^2
-[Fnl_hat_real,Fnl_hat_imag]=...
-    EvaluateSlowForceCoefficient(Phi_hat,N,H,sys);
-
-% Evaluate the real an imaginary part of the slow coupling forces
-% in the time domain and get Fourier coefficients of dynamic components 
-% force coefficients without scaling of kappa_c
-[Fc_hat_real,Fc_hat_imag]=...
-    EvaluateSlowCouplingForceCoefficient(Phi_hat,Gamma_hat,theta_k_mod,...
-                                        theta_k_0,N,H);
-
-% Evaluate the real an imaginary part of the forcing term
-% in the time domain and get Fourier coefficients
-[Ff_hat_real,Ff_hat_imag]=...
-    EvaluateSlowExcitationForceCoefficient(Gamma_hat,N,H);
-
-% Evaluate the real an imaginary part of the derivative term without
-%sclaing of 2*(1-epsilon_a)
-Fd_hat_real= ...
-    EvaluateSlowDerivativeForceCoefficient(Phi_hat,Gamma_hat,N,H);
+[F_hat] = EvaluateSlowForceCoefficient(Phi_hat,N,H,sys);
 
 
 % Build residual equation
 
 % Harmonics
-h = (0:H)';
+h = (-H:H)';
 
-% Linear component stiffnesses
-sRe = repmat((-exc.harmonic.r^2*(1-sys.epsilon_a) + 1 + 2*sys.kappa_c),...
-    [H+1,1]);
-sIm = sys.C(1,1)*exc.harmonic.r + ...
-      2*(1-sys.epsilon_a)*exc.harmonic.r*Omega*1i*h;
+% Sectors
+j = 0:(sys.N_s-1);
 
-% Complex residual
-complex_residual_Re = sRe.*Phi_hat +...
-
-complex_residual_Im = sIm.*Phi_hat + ...
+% Complex stiffness of Harmonics h
+Sh = -(1-sys.epsilon_a)*r^2 + 1 + 2*sys.kappa_c...
+     -2*sys.kappa_c*cos(theta_k_mod*h + theta_k_0) ...
+     - 2*r*Omega*(1-sys.epsilon_a)*h ...
+     + 1i*r*(exp(1i*(theta_k_mod*h + theta_k_0)*j)*sys.C(:,1));
 
 % Complex residual
-complex_stiffness = sys.C(1,1)*r+2*1i*(r*Omega*(1-sys.epsilon_a)*h-...
-    sys.kappa_c*sin(theta_k_mod*h)*sin(theta_k_0));
-
-% Complex residual
-complex_residual = complex_stiffness.*Phi_hat +...
-        8*sys.epsilon_a*r^2/pi^2 * F_hat_imag - eye(H+1,1);
+complex_residual = Sh.*Phi_hat -...
+                   8*sys.epsilon_a*(r/pi)^2 * F_hat - ...
+                   circshift(eye(2*H+1,1),H,1);
 
 % Split into real and imaginary component
-residual = [real(complex_residual(1:(H+1))); ...
-            imag(complex_residual(2:(H+1)))];
+residual = [real(complex_residual); ...
+            imag(complex_residual)];
 
 end
